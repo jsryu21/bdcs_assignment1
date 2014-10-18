7e32eba997b85e544f67cdb6c661c59e83c195bf
wyLee <wylee.xyzi@gmail.com>
2014-10-18 17:17:37 +0900

    Add an empty Client and Driver.

---
+++ src/main/java/org/apache/reef/ml/als/AlsClient.java
@@ -0,0 +1,7 @@
+package org.apache.reef.ml.als;
+
+/**
+ * The Client for Alternating Least Squares ML.
+ */
+public class AlsClient {
+}
---
+++ src/main/java/org/apache/reef/ml/als/AlsDriver.java
@@ -0,0 +1,11 @@
+package org.apache.reef.ml.als;
+
+import com.microsoft.tang.annotations.Unit;
+
+/**
+ * The Driver for Alternating Least Squares ML.
+ */
+@Unit
+public class AlsDriver {
+
+}
